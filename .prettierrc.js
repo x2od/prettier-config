6f8a2a039da8abf15dab7895d65346a9282e5351
David Schach <636977+dschach@users.noreply.github.com>
2024-05-14 13:43:08 -0700

    feat: update dependencies and some config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,15 @@
+const { options } = require('prettier-plugin-apex');
+
 module.exports = {
 	...require('@x2od/prettier-config'),
 	useTabs: true,
 	tabWidth: 2,
 	printWidth: 80,
 	$schema: 'https://json.schemastore.org/prettierrc',
-	overrides: [{ files: '*.{yaml,yml}', useTabs: false }]
+	overrides: [
+		{ 
+			files: '*.{yaml,yml}', 
+			options: { useTabs: false}
+		}
+	]
 };
