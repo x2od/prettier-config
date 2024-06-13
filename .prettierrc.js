6913d7f30841eead0e079fe63fc3d944c605c86c
David Schach <636977+dschach@users.noreply.github.com>
2024-06-13 15:35:22 -0700

    feat(prettier): update json and local js config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,15 +1,12 @@
-const { options } = require('prettier-plugin-apex');
-
 module.exports = {
-	...require('@x2od/prettier-config'),
+	...require("@x2od/prettier-config"),
 	useTabs: true,
 	tabWidth: 2,
-	printWidth: 80,
 	$schema: 'https://json.schemastore.org/prettierrc',
 	overrides: [
 		{ 
 			files: '*.{yaml,yml}', 
-			options: { useTabs: false}
+			options: { useTabs: true}
 		}
 	]
 };
