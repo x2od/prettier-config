aa471227574afdd18a0c7a0e8e8d067e7b7a1c11
David Schach <dschach@x2od.com>
2024-06-16 15:25:14 -0700

    remove unneeded local overrides

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,12 +1,6 @@
 module.exports = {
 	...require("@x2od/prettier-config"),
-	useTabs: true,
-	tabWidth: 2,
 	$schema: 'https://json.schemastore.org/prettierrc',
 	overrides: [
-		{ 
-			files: '*.{yaml,yml}', 
-			options: { useTabs: true}
-		}
 	]
 };
