9bc9f0614ecc95a2a63d6aaa611acac5ac59916f
David Schach <dschach@x2od.com>
2024-06-16 21:14:38 -0700

    build: redo Prettier config files

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,23 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
 module.exports = {
-	...require("@x2od/prettier-config"),
+	...require('@x2od/prettier-config'),
 	$schema: 'https://json.schemastore.org/prettierrc',
 	overrides: [
+		{
+			files: 'index.json',
+			options: {
+				singleQuote: false,
+				printWidth: 80
+			}
+		},
+		{
+			files: '.prettierrc.js',
+			options: {
+				singleQuote: true
+			}
+		}
 	]
 };
